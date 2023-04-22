53436aa0c14371ab0adfe51eb89f71e558dbec9d
Kulikov Alexander <shouter84@yandex.ru>
2023-04-22 12:18:15 +0300

    Added Task003_OneNumber

---
+++ Task003_OneNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №5. Напишите программу, которая на 
+// вход принимает одно число (N), а на выходе 
+// показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+// Console.WriteLine("Enter your number, please");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int i = -N;
+// while (i <= N)
+// {
+//     Console.Write (i + " ");
+//     i++;
+// }
+
+Console.WriteLine("Enter your number, please");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for(int i = -number; i <= number; i++)
+{
+    System.Console.Write(i + " ");
+}
