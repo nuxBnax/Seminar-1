711fd88fb968d9bde51e700a069dbb1dfc021ead
Kulikov Alexander <shouter84@yandex.ru>
2023-04-22 12:39:00 +0300

    Added Task003_OneNumber update

---
+++ Task004_3xNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+//  456 -> 6
+//  782 -> 2
+//  918 -> 8
+
+Console.WriteLine("Enter Triple number: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine(A % 10);
+if (A > 99 && A<1000)
+{
+    Console.WriteLine(A % 10);
+}
+else
+{
+    Console.WriteLine("Error!");
+}
